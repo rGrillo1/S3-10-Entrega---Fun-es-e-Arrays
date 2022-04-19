9a92370e9acb598fed667b8145fe74855abd32c9
Rafael Grillo <rafael.grillo@forsoft.org.br>
2022-04-19 15:10:46 -0300

    S3-10 | 🏁 Entrega - Funções e Arrays

---
+++ FunEArray.js
@@ -0,0 +1,23 @@
+function position (primeiroLugar, segundoLugar,terceiroLugar) {
+
+    const corrida = ['Daniel', 'Rafael', 'Manoel']
+
+    for (i = 0; i < corrida.length; i++) {
+        if (corrida[i] = 'Daniel') {
+           primeiroLugar = corrida[i]
+        }  if (corrida[i] = 'Rafael') {
+          segundoLugar = corrida[i]
+       }  if (corrida[i] = 'Manoel') {
+        terceiroLugar = corrida[i]
+     }
+
+
+  }
+   console.log(primeiroLugar + ' Este é o primeiro lugar') 
+   console.log(segundoLugar + ' Este é o segundo lugar')  
+   console.log(terceiroLugar + ' Este é o segundo lugar') 
+   
+    
+ }
+
+ position()
